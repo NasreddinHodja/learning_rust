a39a9856e68246ddb1d45b482f0c726f7686246b
Tomás Bizet <tbizetde@gmail.com>
2021-07-01 20:32:32 -0300

    chap 11

---
+++ examples/adder/target/debug/deps/adder-ab9cc08561ab9610.d
@@ -0,0 +1,5 @@
+/home/nasreddin/prog/learning_rust/examples/adder/target/debug/deps/adder-ab9cc08561ab9610: src/lib.rs
+
+/home/nasreddin/prog/learning_rust/examples/adder/target/debug/deps/adder-ab9cc08561ab9610.d: src/lib.rs
+
+src/lib.rs:
---
+++ examples/adder/target/debug/deps/adder-f9ea6314e547d7ca.d
@@ -0,0 +1,7 @@
+/home/nasreddin/prog/learning_rust/examples/adder/target/debug/deps/adder-f9ea6314e547d7ca.rmeta: src/lib.rs
+
+/home/nasreddin/prog/learning_rust/examples/adder/target/debug/deps/libadder-f9ea6314e547d7ca.rlib: src/lib.rs
+
+/home/nasreddin/prog/learning_rust/examples/adder/target/debug/deps/adder-f9ea6314e547d7ca.d: src/lib.rs
+
+src/lib.rs:
